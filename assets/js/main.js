fb0bb9726fdd5f9d52cd33f89186ebbb2d4fd29b
Antonio <antonio.lovotrico@icloud.com>
2020-11-19 15:59:36 +0100

    completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,21 @@
 //Scrivi un programma che stampi i numeri da 1 a 100
 
-// for (i = 0; i < 100; i++) {
-//     //console.log(i + 1);
-//     document.getElementById("num").innerHTML = i + 1;
-//     //multipli di 3 stampi “Fizz” al posto del numero
-//     // if (i % 3 == 0) {
-//     //     document.getElementById("num").innerHTML = "Fizz";
-//     // }
-
-// }
-var multi3 = false;
-var multi5 = false;
-var multi3e5 = false;
-
-var points = new Array(100);
-for (var i = 0; i < 100; i++) {
-    points[i] = i + 1;
+for (var i = 1; i <= 100; i++) {
+ 
     console.log(i);
-    if (points[i] % 3 == 0) {
-        multi3 = true;
-     } else if (points[i] % 5 == 0) {
-        multi5 = true;
-     }
-    
+    var elencoNumeri = document.getElementById("num").innerHTML;
+     
+    //multipli di 3 e 5 stampa “FizzBuzz” al posto del numero
+    if (i % 15 == 0) {
+        document.getElementById("num").innerHTML = elencoNumeri + "<li>fizzBuzz</li>";
+    //multipli di 3 stampa “Fizz” al posto del numero
+    }else if (i  % 3 == 0) {
+        document.getElementById("num").innerHTML = elencoNumeri + "<li>Fizz</li>";
+    //multipli di 5 stampa “Buzz” al posto del numero
+    } else if (i % 5 == 0) {
+        document.getElementById("num").innerHTML = elencoNumeri + "<li>Buzz</li>";
+    //Stampa  numero  
+    } else {
+        document.getElementById("num").innerHTML = elencoNumeri + "<li>" + i  + "</li>";
+    }
 }
-if (multi3) {
-    console.log("Fizz");
-} else if (multi5) {
-    console.log("Buzz");
-}
-
-//document.getElementById("num").innerHTML = "Fizz";
-
-//else if (points[i] % 5 == 0) {
-    //     document.getElementById("num").innerHTML = "Buzz";
-    // } else {
-    //     document.getElementById("num").innerHTML = points;
-    // }
